05b703d73982cfee551e0cf8c445bad95f070253
Dmitrii Zhuk <dmitry.v.zhuk@gmail.com>
2022-08-30 16:11:38 +0300

    Task 01 complete

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень: ");
+int pow = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < pow; i++)
+{
+    result = result * num;
+}
+
+Console.WriteLine("Результат возведения числа " + num + " в степень " + pow + " = " + result);
+
